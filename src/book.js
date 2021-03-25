4ede2f24069f37c4577e08250108df1bc5de8549
Claire Fields <clairefields15@gmail.com>
2021-03-24 19:57:05 -0600

    createTitle, buildMainCharacter, saveReview all pass

--- src/book.js
+++ src/book.js
@@ -1,11 +1,39 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
 }
+
+function buildMainCharacter(name,age,pronouns) {
+  return {
+  name: name,
+  age: age,
+  pronouns: pronouns,
+}
+}
+
+//create an array for reviews
+var reviews = [];
+
+//a function that adds reviews to the array "reviews"
+// function saveReview(reviewText, reviews) {
+// return reviews.push (reviewText)
+// }
+
+//before adding a new review, loop through the array and check for duplicates while looping
+
+function saveReview(reviewText, reviews) {
+  if (reviews.includes(reviewText)){
+  return
+  }else{return reviews.push (reviewText)
+  }
+}
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
