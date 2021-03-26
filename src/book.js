84d17260a03ff586cc99e362d3eca24e8a997268
Claire Fields <clairefields15@gmail.com>
2021-03-25 20:28:52 -0600

    pass all testsand refactorgit status

--- src/book.js
+++ src/book.js
@@ -4,21 +4,13 @@ function createTitle(bookTitle) {
 
 function buildMainCharacter(name,age,pronouns) {
   return {
-  name: name,
-  age: age,
-  pronouns: pronouns,
+  name,
+  age,
+  pronouns,
 }
 }
 
-//create an array for reviews
 var reviews = [];
-
-//a function that adds reviews to the array "reviews"
-// function saveReview(reviewText, reviews) {
-// return reviews.push (reviewText)
-// }
-
-//before adding a new review, loop through the array and check for duplicates while looping
 
 function saveReview(reviewText, reviews) {
   if (reviews.includes(reviewText)){
@@ -27,23 +19,23 @@ function saveReview(reviewText, reviews) {
   }
 }
 
-//calculate page count function
-//get number of characters in title
-//multiply characters by 20 to add 20 pages per letter
-//tried first with createTitle(bookTitle).length and did not work, why?
 function calculatePageCount(bookTitle) {
-  bookTitle = bookTitle.length * 20;
-  return bookTitle
+  pageCount = bookTitle.length * 20;
+  return pageCount
 }
 
-function writeBook(title,mainCharacter,genre) {
+function writeBook(bookTitle,mainCharacter,genre) {
 var book = {
-  title: title,
-  mainCharacter: mainCharacter,
-  genre: genre,
-  pageCount: calculatePageCount(title)
+  title: bookTitle,
+  mainCharacter,
+  genre,
+  pageCount: calculatePageCount(bookTitle)
 }
 return book
+}
+
+function editBook(book) {
+  return book.pageCount= book.pageCount * 0.75
 }
 
 module.exports = {
@@ -52,5 +44,5 @@ module.exports = {
   saveReview,
   calculatePageCount,
   writeBook,
-  // editBook
+  editBook
 }
