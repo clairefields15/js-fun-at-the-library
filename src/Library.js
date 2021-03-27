06c261ec4b16d958b01cd20675b41e3996ef2860
Claire Fields <clairefields15@gmail.com>
2021-03-27 13:03:18 -0600

    pass all tests except only checkout if book is on shelf

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,34 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+//shelf object with three arrays (fantasy, fiction, nonfiction)
+    shelves: {
+      fantasy: [],
+      fiction:[],
+      nonFiction:[],
+    }
+  }
+  return library
+}
+
+function addBook(library, bookTitle) {
+  library.shelves[bookTitle.genre].push(bookTitle)
+}
+
+//brackets allow you to use a access properties dynamically!!
+function checkoutBook(library, bookTitle, genre) {
+//iterate through the genre array
+for(var i = 0 ; i < genre.length ; i++) {
+  //access the genre array in the object library and remove the bookTitle from the array
+    library.shelves[genre].splice(bookTitle[i], 1)
+  }
+  //return a string "you have checked out"
+  return `You have now checked out ${bookTitle} from the ${library.name}`
+}
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
