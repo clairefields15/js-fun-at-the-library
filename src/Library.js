28911642c8fd83b73a63887be6510e950cac95b3
Claire Fields <clairefields15@gmail.com>
2021-03-28 18:57:41 -0600

    finish library.js and refactor

--- src/Library.js
+++ src/Library.js
@@ -15,22 +15,19 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book)
 }
 
-//this passes first test but not second
 function checkoutBook(library, bookTitle, genre) {
   //loops though the genre array
 for(var i = 0 ; i < library.shelves[genre].length ; i++) {
   var title = library.shelves[genre][i].title
-  console.log("this is loop number >>" + i)
-  //if title at that index matches bookTitle
   if (title === bookTitle) {
     //remove it from the array and check it out
-    library.shelves[genre].splice(genre[i], 1)
+    library.shelves[genre].splice(i, 1)
       return `You have now checked out ${bookTitle} from the ${library.name}`
-    } else {
-      return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
     }
   }
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
 }
+
 
 
 module.exports = {
